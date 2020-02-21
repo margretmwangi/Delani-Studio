2b00d1e8be054674c84082f6843434cfd669338f
margaret mwangi <margaret13mwangi@gmail.com>
2020-02-21 17:11:30 +0300

    initial commit

---
+++ Read-me.md
@@ -0,0 +1,14 @@
+# {Delani-Studio}
+#### {This portfolio provide information about the studio including the services offered and their contacts to enhance the communication}, {20/02/2020}
+#### By **{Margret mwangi}**
+## Description
+{This is a detailed description of your application, including its purpose and usage.  Give as much detail as needed to explain what the application does, and any other information you want users or other developers to have. }
+## Setup/Installation Requirements
+* ###Technologies Used
+This app was created using HTML, Javascript, CSS(Bootstrap), and jQuery functions
+## Support and contact details
+Incase of any arising question or feedback contact me using this email:margaret13mwangi@gmail.com
+{### License
+*{This app is licensed under the MIT license group}*
+Copyright (c) {2020} **
+  
---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    //Portfolio label on hover effect
+    $('#portfolio span').addClass("hide");
+    $('#portfolio .doings').hover(function() {
+      /*fade in code*/
+      $(this).find('span').removeClass('hide');
+      $(this).find('.doings').addClass('imageEffect');
+    }, function() {
+      /*fade out code*/
+      $(this).find('span').addClass('hide');
+      $(this).find('.doings').removeClass('imageEffect');
+    });
+    //SubmissionForm
+    $('#submit').submit(function(event) {
+      var name = $('#name').val();
+      var email = $('#email').val();
+      var message = $('#message').val();
+      if (name == "" || email == "" || message == "") {
+        $("#message").text("Please fill in all the fields with correct data.");
+        $('#message').removeClass("successful");
+        $('#message').addClass("failed");
+      } else {
+        $("#message").text("Thank you " + name + ".<br>Your message has been successfully submitted.");
+        $('#message').removeClass("failed");
+        $('#message').addClass("successful");
+      }
+      event.preventDefault();
+    });
+      //What we do description toggle
+      $(".first").click(function() {
+        $(".illustration1").toggle(500);
+        $(".details1").toggle(500);
+      });
+      $(".second").click(function() {
+        $(". further illustration2").toggle(500);
+        $(".more details2").toggle(500);
+      });
+      $(".third").click(function() {
+        $(". further illustration3").toggle(500);
+        $(".more details3").toggle(500);
+      });
+    //Scroll to 'About Section' feature on mouse click
+    $(function() {
+      $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+          scrollTop: $($(this).attr('href')).offset().top
+        }, 250, 'linear');
+      });
+    });
+  });
+  
+  
